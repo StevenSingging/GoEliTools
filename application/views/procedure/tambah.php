6949e8284e8c441e5dc31f4a908ed462f6122edb
StevenSingging <89833685+StevenSingging@users.noreply.github.com>
2022-10-13 14:18:59 +0700

    Update tambah.php

--- application/views/procedure/tambah.php
+++ application/views/procedure/tambah.php
@@ -99,11 +99,11 @@
 <div class="form-group row">
   <label for="procedure" class="col-sm-3 text-right"></label>
   <div class="col-sm-9">
-    <button type="btn btn-success" type="submit" name="submit" value="submit">
-      <i class="fa fa-save"></i> Simpan Data
-    </button>
-    <button type="btn btn-default" type="reset" name="reset" value="reset">
-      <i class="fa fa-times"></i> Reset
+     <button type="submit" class="btn btn-primary">
+                <i class="fa fa-save"></i> Simpan Data
+     </button>
+    <button type="reset" class="btn btn-info">
+                <i class="fa fa-times"></i> Reset
     </button>
     
   </div>
